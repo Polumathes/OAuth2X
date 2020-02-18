747959abe240bba9b428a809216be5050cbfa67c
gsm-josh <joshua@greyskymedia.com>
2020-02-18 15:40:54 -0500

    added system settings

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -35,4 +35,34 @@ $systemSettings[1]->fromArray(array (
   'value' => '1',
   'area' => '',
 ), '', true, true);
+$systemSettings[2] = $modx->newObject('modSystemSetting');
+$systemSettings[2]->fromArray(array (
+    'key' => 'authorizeUrl',
+    'name' => 'Authorize URL',
+    'description' => 'OAuth2X Authorize URL',
+    'namespace' => 'oauth2x',
+    'xtype' => 'textfield',
+    'value' => '',
+    'area' => '',
+), '', true, true);
+$systemSettings[3] = $modx->newObject('modSystemSetting');
+$systemSettings[3]->fromArray(array (
+    'key' => 'oauth2server_login_context',
+    'name' => 'Login Context',
+    'description' => 'OAuth2X Login Context',
+    'namespace' => 'oauth2x',
+    'xtype' => 'modx-combo-context',
+    'value' => '',
+    'area' => '',
+), '', true, true);
+$systemSettings[4] = $modx->newObject('modSystemSetting');
+$systemSettings[4]->fromArray(array (
+    'key' => 'tokenControllerUrl',
+    'name' => 'Token Controller URL',
+    'description' => 'Token Controller URL',
+    'namespace' => 'oauth2x',
+    'xtype' => 'textfield',
+    'value' => '',
+    'area' => '',
+), '', true, true);
 return $systemSettings;
